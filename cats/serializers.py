0cddc9b3b413f41fc1ff2e6998b9b19370b1f0c4
idotsenko <idotsenko@usergate.com>
2026-01-18 17:46:36 +0300

    Валидация на уровне поля

--- cats/serializers.py
+++ cats/serializers.py
@@ -35,6 +35,14 @@ class CatSerializer(serializers.ModelSerializer):
                   'age')
         read_only_fields = ('owner',)
 
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value 
+
+
+
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
 
